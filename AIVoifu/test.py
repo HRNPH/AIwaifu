5565752590e3f83e9c7588317d17b4afdad58a58
HRNPH <hrnph@protonmail.com>
2023-10-19 06:05:26 +0700

    new test file

--- AIVoifu/test.py
+++ AIVoifu/test.py
@@ -2,4 +2,6 @@ from tts.tts import auto_tts
 
 tts_model = auto_tts(model_selection="gtts")
 # tts_model.tts('おはよごさいます わたしは さいとちゃんですは', "test.wav", language='en')
-tts_model.tts('Hello World', "test.wav", language='en')
+# text = """My name is Lilia. I'm a 18 year old elf from the far north. I've been traveling with a group of adventurers for a while, and we're on our way to the high mountain peak."""
+text = "もう少し食べたいです"
+tts_model.tts(text, "test.wav", language='jp')
